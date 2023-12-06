f3c8e93f3006b06e137e58ad6d4b8624da71c07d
Rohit3003T <152482885+Rohit3003T@users.noreply.github.com>
2023-12-06 23:58:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+ var pincode;
+
+function resp(){
+    var num=document.getElementById("input1");
+    pincode = num.value;
+    alert("sorry service is not avolable at" + pincode +" pincode");
+    
+
+
+}
+function refreshPage() {
+    location.reload();
+  }
+  function sorry(){
+  alert("sorry service is not avilable at this time ")
+
+  }
+ 
